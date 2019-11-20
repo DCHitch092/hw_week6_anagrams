396909159a85b5b037910643c35509216a062142
Richard <fahitch+github@gmail.com>
2019-11-20 19:56:42 +0000

    stuck on isograms

--- hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,45 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.split("");
+  // console.log(this.word);
 }
 
 IsogramFinder.prototype.isIsogram = function () {
+  let allLetters = [];
+  console.log(this.word);
 
-}
+  allLetters = this.word.map((letter) => {
+    if (!allLetters.includes(letter)){
+      return letter;
+    } else if ( allLetters.includes(letter)){
+      return false;
+    }
+    console.log(allLetters);
+    return allLetters;
+  });
+
+
+  // let allLetters = this.word.reduce((letters, letter) => {
+  //   if(!letters.includes(letter)){
+  //     console.log(letter);
+  //     letters + letter;
+  //   } else {
+  //     return false;}
+  //
+  // },"");
+  //
+  // console.log(allLetters);
+  // return true;
+
+//  for (let letter of this.word){
+//    if (letters.includes(letter)) {
+//      return false;
+//    } else {
+//      console.log(letter);
+//      letters.push(letter);
+//    }
+//  }
+//   return true;
+// };
+};
 
 module.exports = IsogramFinder;
